841d7889baf5a88a6f5a7eaf738ff349f8742d04
mrwhoknows55 <avdhuttanugade55@gmail.com>
2022-11-06 00:01:01 +0530

    add: camera permission check

--- app/src/main/java/com/mrwhoknows/wallet/address/validator/ui/ValidationFragment.kt
+++ app/src/main/java/com/mrwhoknows/wallet/address/validator/ui/ValidationFragment.kt
@@ -1,16 +1,26 @@
 package com.mrwhoknows.wallet.address.validator.ui
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
+import com.mrwhoknows.wallet.address.validator.R
 import com.mrwhoknows.wallet.address.validator.databinding.FragmentValidationBinding
 
 class ValidationFragment : Fragment() {
@@ -28,9 +38,13 @@ class ValidationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.setAddressType(addressType)
-        qrCodeScannerLauncher.launch(getQrScanOptions())
         setupObservables()
         setupClickListeners()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkAndAskCameraPermission()
     }
 
     private fun setupClickListeners() {
@@ -80,8 +94,71 @@ class ValidationFragment : Fragment() {
         result.contents?.let {
             viewModel.createQRCodeBitmap(result.contents)
         } ?: kotlin.run {
-            findNavController().navigateUp()
+            navigateBack()
         }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            qrCodeScannerLauncher.launch(getQrScanOptions())
+        }
+    }
+
+    private fun checkAndAskCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                qrCodeScannerLauncher.launch(getQrScanOptions())
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(), Manifest.permission.CAMERA
+            ) -> {
+                showPermissionRationaleDialog(false)
+            }
+
+            ContextCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_DENIED -> {
+                showPermissionRationaleDialog(true)
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.CAMERA
+                )
+            }
+        }
+    }
+
+    private fun showPermissionRationaleDialog(deniedAlways: Boolean) {
+        MaterialAlertDialogBuilder(requireContext()).setTitle(R.string.permission_required)
+            .setMessage(R.string.rationale_msg).setCancelable(false)
+            .setPositiveButton(R.string.ok) { _, _ ->
+                if (!deniedAlways) {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.CAMERA
+                    )
+                } else {
+                    Intent(
+                        ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.parse("package:${requireActivity().packageName}")
+                    ).apply {
+                        addCategory(Intent.CATEGORY_DEFAULT)
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(this)
+                    }
+                }
+            }.setNegativeButton(R.string.return_to_home) { _, _ ->
+                navigateBack()
+            }.show()
+    }
+
+    private fun navigateBack() {
+        findNavController().navigateUp()
     }
 
     companion object {
